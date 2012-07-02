bb317a660d41d7717698ec4a6072f41989df7fd9
Carlos Cardona <cgcardona@gmail.com>
2012-07-02 13:31:38 -0700

    add links to the work page

--- includes/nav_work.php
+++ includes/nav_work.php
@@ -1,25 +1,25 @@
       <div id='work' class="">
         <div class="row-fluid ">
           <div class="span4">
-            <h2>Audiofile</h2>
+            <h2><a href='https://audiofile.cc/'>Audiofile</a></h2>
             <p>The primary goal of this project is to define a convention for creating sheet music using HTML5 custom data attributes which describes the parent/child/sibling relationship between all symbols on a musical document thus allowing the construction of a Document Object Model. A secondary goal is to create a suite of jQuery functions that allow the user to traverse and manipulate this musical DOM.
           </div>
-          <div class="span8"><img src='img/audiofile1.png' alt='Audiofile'></div>
+          <div class="span8"><a href='https://audiofile.cc/'><img src='img/audiofile1.png' alt='Audiofile'></a></div>
         </div>
         <hr>
         <div class="row-fluid ">
           <div class="span4">
-            <h2>Boombox.js</h2>
+            <h2><a href='https://audiofile.cc/boombox/'>Boombox.js</a></h2>
             <p>The primary goal of this project is to create a high quality HTML5 audio boombox that can be easily skinned and extended.
           </div>
-          <div class="span8"><img src='img/audiofile2.png' alt='Boombox.js'></div>
+          <div class="span8"><a href='https://audiofile.cc/boombox/'><img src='img/audiofile2.png' alt='Boombox.js'></a></div>
         </div>
         <hr>
         <div class="row-fluid ">
           <div class="span4">
-            <h2>Boombox for Chrome™</h2>
+            <h2><a href='https://audiofile.cc/chrome/'>Boombox for Chrome™</a></h2>
             <p>Boombox for Chrome™ by Audiofile is a new extension for the Chrome browser. It leverages the amazing functionality of HTML5 along with the extensive API provided by Chrome to give you the ability to bookmark boomboxes and to listen to them no matter where you are on the web.
           </div>
-          <div class="span8"><img src='img/boomboxinterface.jpg' alt='Boombox for Chrome'></div>
+          <div class="span8"><a href='https://audiofile.cc/chrome/'><img src='img/boomboxinterface.jpg' alt='Boombox for Chrome'></a></div>
         </div>
       </div>
