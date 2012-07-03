241d0b689044417f6e4da3914883671e78fcff67
Carlos Cardona <cgcardona@gmail.com>
2012-07-02 18:24:40 -0700

    retina and non retina friendly touch icons as well as launching with no browser chrome if the website is saved to the homescreen

--- index.php
+++ index.php
@@ -7,6 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link rel="icon" href="img/favicon.ico" type="image/ico">
+    <meta name="apple-mobile-web-app-capable" content="yes"> 
+    <meta name="apple-touch-fullscreen" content="yes">
+
+    <!-- For third-generation iPad with high-resolution Retina display: -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.gif">
+    <!-- For iPhone with high-resolution Retina display: -->
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.gif">
+    <!-- For first- and second-generation iPad: -->
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.gif">
+    <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+    <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-precomposed.gif">
+
     <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
